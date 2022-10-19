f5ce95f65d5b4452047992bd8a42c81669ade4c5
DishaGup <guptamansi201@gmail.com>
2022-10-19 18:01:28 +0530

    checkout

--- palindromeOrNot.js
+++ palindromeOrNot.js
@@ -3,9 +3,9 @@ let rev_str="";
     for(let i=N-1;i>=0;i--){
         rev_str+=str[i];
     }
-    if(str>=rev_str){
+    if(str==rev_str){
     console.log("Yes");
     }
     else{
-        console.log("No")
+        console.log("No");
     }
--- primeOrNot.js
+++ primeOrNot.js
@@ -1,11 +1,11 @@
 let count = 0;
-     for(i=1;i<=num;i++){
+     for(let i=1;i<=num;i++){
          if(num%i==0){
              count++;
          }
      }
-     if(count<=2)
-         console.log("Yes");
+     if(count<2){
+         console.log("YES");
      
-     else
-         console.log("No");
+     }else{
+         console.log("NO");}
