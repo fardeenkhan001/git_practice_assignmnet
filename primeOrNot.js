900ce9cded9896d58ece90f1169af2d45ea7f4e8
DishaGup <guptamansi201@gmail.com>
2022-10-19 18:33:05 +0530

    check

--- primeOrNot.js
+++ primeOrNot.js
@@ -1,10 +1,12 @@
+//checking prime number
+let num = 13;
 let count = 0;
-     for(i=1;i<=num;i++){
+     for(let i=1;i<=num;i++){
          if(num%i==0){
              count++;
          }
      }
-     if(count<=2)
+     if(count<2)
          console.log("Yes");
      
      else
