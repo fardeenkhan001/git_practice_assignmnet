54efe6549f1d4be2719a24be7dc6dc4119cf34ac
fardeen <70657851+AlphaMale6967@users.noreply.github.com>
2022-10-19 12:30:06 +0530

    Prime and Palindrome file added

---
+++ palindromeOrNot.js
@@ -0,0 +1,11 @@
+let str = "moon";
+let rev_str="";
+    for(let i=N-1;i>=0;i--){
+        rev_str+=str[i];
+    }
+    if(str>=rev_str){
+    console.log("Yes");
+    }
+    else{
+        console.log("No")
+    }
